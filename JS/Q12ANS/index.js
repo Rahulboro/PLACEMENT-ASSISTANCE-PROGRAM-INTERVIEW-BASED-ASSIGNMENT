03fab9a12670e265b545d924b227530bd7d18d8a
ujjalbordoloi <ujjalbordoloi90@gmail.com>
2023-06-06 23:41:18 +0530

    q12

--- JS/Q12ANS/index.js
+++ JS/Q12ANS/index.js
@@ -1,19 +1,3 @@
-const pics = [];
-const pic = document.querySelectorAll('randomimg');
-
-function showImage() {
-    var a = Math.floor(Math.random() * pics.length);
-    var img = pics[a];
-    pic.style.backgroundImage = img;
-}
-
-setInterval(showImage, 1000);
-
-
-
-
-/////////////
-
 
 let inputBox = document.querySelector('typingfield');
 
